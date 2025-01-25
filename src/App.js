7e4f2af56433152fd34f3f513d7e56c186f758e2
sarthak576 <sarthakguptarjn@gmail.com>
2025-01-25 10:13:21 +0530

    git msg

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+const projectList=["project1","project2","project3"];
+
+  return (
+   <Container>
+
+  
+    <Typography variant='h4' sx={{my:4, textAlign:'center',color:'primary.main',bgcolor:''}}>
+    My Projects
+    </Typography>
+    
+
+
+
+   
+   </Container>
+  );
+}
+
+export default App;
